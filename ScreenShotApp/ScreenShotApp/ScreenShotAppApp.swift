2fb8a8041b485dacec75c3befa81d4d1d9dc72bf
govindrnair <govindrnair@gmail.com>
2025-05-01 15:36:09 +0530

    Initial commit for ScreenShotApp

---
+++ ScreenShotApp/ScreenShotApp/ScreenShotAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenShotAppApp.swift
+//  ScreenShotApp
+//
+//  Created by Govind Nair on 1/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ScreenShotAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScreenShotApp/ScreenShotAppTests/ScreenShotAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenShotAppTests.swift
+//  ScreenShotAppTests
+//
+//  Created by Govind Nair on 1/5/25.
+//
+
+import Testing
+@testable import ScreenShotApp
+
+struct ScreenShotAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
